584270929e34f80f53cb6de783ca65599abe7815
uncleaar <danilyudin723@gmail.com>
2023-10-26 20:29:23 +0600

    tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import { Wrapper } from '@/components'
 
 export default function Home() {
   return (
-    <Wrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>main</Wrapper>
+    <Wrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
+      <div>
+        home
+      </div>
+    </Wrapper>
   )
 }
