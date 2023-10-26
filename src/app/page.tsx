b73eaff6a7c7adbae7dc08bc52430f69cb47b7aa
uncleaar <danilyudin723@gmail.com>
2023-10-26 20:19:27 +0600

    clcs func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
+import { Wrapper } from '@/components'
 
 export default function Home() {
   return (
-    <main>main</main>
+    <Wrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>main</Wrapper>
   )
 }
